611425e807b6dad5f881e0d169b3a51fd6ee0936
hm19890304 <hm19890304@icloud.com>
2022-04-25 21:19:29 +0900

    Active Hashの導入

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,16 @@
 class Video < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
   has_one_attached :movie
+
+  with_options numericality: { other_than: 1, message: "を入力してください" } do
+    validates :category_id
+    validates :prefecture_id
+  end
 
   with_options presence: true do
     validates :title
     validates :explanation
+  end
 end
