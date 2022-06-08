826a243bc0939acfdc57707e7f9b649bf64a8925
hm19890304 <hm19890304@icloud.com>
2022-06-08 22:02:58 +0900

    videoモデルのFactoryBot記述

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :video do
-    
+    title                 { Faker::Lorem.words }
+    explanation           { Faker::Lorem.sentences }
+    prefecture_id         { Faker::Number.between(from: 2, to: 47)}
+    address               { Faker::Address.city }
+    category_id           { Faker::Number.between(from: 2, to: 8)}
+    association :user
+
+    after(:build) do |video|
+      video.movie.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
