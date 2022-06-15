6766024e62fdf029c73d88faddaa5de8b0a8d8f7
hm19890304 <hm19890304@icloud.com>
2022-06-15 22:13:54 +0900

    exampleの記入

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe "GET #show" do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの動画のタイトルが存在する' do 
+    end
+    it 'showアクションにリクエストするとレスポンスにお気に入り動画一覧ボタンが存在する' do 
     end
   end
 end
